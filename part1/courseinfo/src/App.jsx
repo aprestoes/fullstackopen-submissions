2953351982f8f2bb3393007ef887615c516ce8b5
Dan Bala <47873094+aprestoes@users.noreply.github.com>
2025-11-21 23:05:37 -0500

    Completed Exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,19 +6,19 @@ const Header = (props) => {
   );
 }
 
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercises}</p>
+  );
+}
+
 //Assuming that there is always 3 parts and associated numParts
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </div>
   );
 }
@@ -27,7 +27,7 @@ const Total = (props) => {
   return (
     <div>
       <p>
-        Number of exercises {propss.exercises1 + props.exercises2 + props.exercises3}
+        Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
       </p>
     </div>
   );
