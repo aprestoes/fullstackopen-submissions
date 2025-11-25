79cb32293d65e6f751381d3415cbec2d532fd869
Dan Bala <47873094+aprestoes@users.noreply.github.com>
2025-11-24 22:14:33 -0500

    Completed Exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -14,18 +14,43 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [score, setScore] = useState(0);
+
+  const handleGood = () => {
+    setScore(score + 1);
+    const updatedGood = good + 1;
+    setGood(updatedGood);
+    setTotal(updatedGood + neutral + bad);
+  };
+
+  const handleNeutral = () => {
+    const updatedNeutral = neutral + 1;
+    setNeutral(updatedNeutral);
+    setTotal(updatedNeutral + good + bad);
+  };
+
+  const handleBad = () => {
+    setScore(score - 1);
+    const updatedBad = bad + 1;
+    setBad(updatedBad);
+    setTotal(updatedBad + neutral + good);
+  }
 
   return (
     <div>
       <h1>Give Feedback</h1>
-      <Button onClick={() => {setGood(good + 1)}} text="Good" />
-      <Button onClick={() => {setNeutral(neutral + 1)}} text="Neutral" />
-      <Button onClick={() => {setBad(bad + 1)}} text="Bad" />
+      <Button onClick={handleGood} text="Good" />
+      <Button onClick={handleNeutral} text="Neutral" />
+      <Button onClick={handleBad} text="Bad" />
 
       <h1>Statistics</h1>
       <Display counter={good} text="Good: " />
       <Display counter={neutral} text="Neutral: " />
       <Display counter={bad} text="Bad: " />
+      <Display counter={total} text="All: " />
+      <Display counter={parseFloat(score / total)} text="Average: " />
+      <Display counter={parseFloat(good / total)} text="Positive: " />
 
     </div>
   )
