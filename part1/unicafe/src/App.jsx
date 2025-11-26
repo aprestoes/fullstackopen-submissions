7680e22fc1ab4f3fbdfea3476e89292c8c1e959a
Dan Bala <47873094+aprestoes@users.noreply.github.com>
2025-11-25 20:49:18 -0500

    Completed Exercise 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,9 +3,10 @@ import { useState } from 'react'
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 const StatisticLine = ({ value, text }) => {
   return (
-    <div>
-      <p><b>{text}</b> {value}</p>
-    </div>
+    <tr>
+      <td><b>{text}</b></td>
+      <td>{value}</td>
+    </tr>
   );
 };
 
@@ -16,12 +17,16 @@ const Statistics = (props) => {
     return (
       <div>
         <h1>Statistics</h1>
-        <StatisticLine value={props.stats.good} text="Good: " />
-        <StatisticLine value={props.stats.neutral} text="Neutral: " />
-        <StatisticLine value={props.stats.bad} text="Bad: " />
-        <StatisticLine value={props.stats.total} text="All: " />
-        <StatisticLine value={parseFloat(props.stats.score / props.stats.total)} text="Average: " />
-        <StatisticLine value={parseFloat(props.stats.good / props.stats.total)} text="Positive: " />
+        <table>
+          <tbody>
+            <StatisticLine value={props.stats.good} text="Good: " />
+            <StatisticLine value={props.stats.neutral} text="Neutral: " />
+            <StatisticLine value={props.stats.bad} text="Bad: " />
+            <StatisticLine value={props.stats.total} text="All: " />
+            <StatisticLine value={parseFloat(props.stats.score / props.stats.total)} text="Average: " />
+            <StatisticLine value={Math.round((props.stats.good / props.stats.total) * 100) + "%"} text="Positive: " />
+          </tbody>
+        </table>
       </div>
   );
   }
